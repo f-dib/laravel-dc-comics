7300f3a202873ca7f5931ccf37e05646de97bf78
federico-dibitonto <f.dibitonto94@gmail.com>
2024-04-11 16:36:56 +0200

    created table column, comics.php in config and seeder

--- database/migrations/2024_04_11_141337_create_comics_table.php
+++ database/migrations/2024_04_11_141337_create_comics_table.php
@@ -14,7 +14,15 @@ return new class extends Migration
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
 
-            
+            $table->string('title');
+            $table->text('description');
+            $table->string('thumb')->nullable();
+            $table->decimal('price', 5, 2);
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type');
+            $table->string('artists');
+            $table->string('writers');
 
             $table->timestamps();
         });
